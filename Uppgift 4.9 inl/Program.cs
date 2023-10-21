67345d4fa6aaae6116defbdff83ad01a4687f3a9
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 18:16:02 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Uppgift 4.9 inl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in sidlängden på treangeln");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input; i++)
+            {
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");   
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
